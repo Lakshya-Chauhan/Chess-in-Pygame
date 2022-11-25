09269f77c051949d0642a2f962b98f88c84865a4
Legendary <99966559+Legendary3995@users.noreply.github.com>
2022-11-25 18:41:25 +0530

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,4 +1,6 @@
-class piece:
+class chess:
+  white = []
+  black = []
 
   def __init__(self, init_pos: list, cost: int, surface, color: int,
                piece: int, number: int):
@@ -6,16 +8,26 @@ class piece:
     self.pos = init_pos  #Stores the current Position of the chess piece
     self.cost = cost  #Stores the cost of Chess piece
     self.surface = surface  #Stores the surface to blit for representing chess piece
-    self.color = color  #stores color of chess piece: 0 - White, 1 - Black
+    self.color = color  #stores color of chess piece: (-1) - White, 1 - Black
     self.piece = piece  #0 - King, 1 - Queen, 2 - Rook, 3 - Bishop, 4 - Knight, 5 - Pawn
     self.temp_pos = [
     ]  #stores the temporary position of chess, made to be used in Drag and Drop and legal move
     self.captured = False  #variable tells if the piece is captured
     self.number = number
 
+    if self.color == -1:
+      chess.white.append(self)
+    else:
+      chess.black.append(self)
+
   #returns the set of all positions of given color, else give all other position of the color given except the exception(the exception's number is given) given
   def occupied_pos(color: int, _except=None):
-    pass
+    if color == -1:
+      return [tuple(i.pos) for i in chess.white if i.number != _except]
+      #returns the list of all the white positions except the exception
+    else:
+      return [tuple(i.pos) for i in chess.black if i.number != _except]
+      #returns the list of all the black positions except the exception
 
   #if type is True, it means that it is needed to check if the given position is true, else it means to return a set of legal moves
   def legal_moves(self, type: bool, position=tuple()):
@@ -61,9 +73,8 @@ class piece:
             #logic for finding legal move for Knight
             if abs(self.pos[0] - x) != abs(self.pos[1] - y):
               #exception of logic
-              legal_moves.add((x, y))
-          else:
-            continue
+              if (x, y) not in chess.occupied_pos(self.color, self.number):
+                legal_moves.add((x, y))
     elif self.piece == 5:  #legal move of Pawn
       pass
     else:
